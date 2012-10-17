4d0c76d0dab882356cfccc89b46be78514ef9aaa
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2012-10-17 07:12:53 -0400

    [saleem] Fixed the erroneous description in the Javadoc for the Filter interface.

--- src/com/saleem_siddiqui/java/util/Filter.java
+++ src/com/saleem_siddiqui/java/util/Filter.java
@@ -5,8 +5,8 @@ package com.saleem_siddiqui.java.util;
  * The filter interface, which determines if a given event should be accepted or rejected by the Observer
  * <p/>
  * Implementing classes <b>should</b> strive to provide a stateless implementation of the accept() method. That is,
- * the following <b>should</b> always be true, for two different event objects A and B and an Filter object F:
- * A.equals(B) == (F.accept(A) == F.accept(B))
+ * the following <b>should</b> always be true, for two different event objects A and B and a Filter object F:
+ * IF A.equals(B) THEN (F.accept(A) == F.accept(B))
  */
 public interface Filter<E> {
     boolean accept(E event);
